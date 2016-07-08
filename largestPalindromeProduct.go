158572c6927933783faad511d7e1031ad2bf6415
Jasmine Noack <jasminenoack@gmail.com>
2016-07-07 22:12:32 -0400

    adds testing go routines

--- largestPalindromeProduct.go
+++ largestPalindromeProduct.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"runtime"
 	"strconv"
 )
 
@@ -33,6 +34,46 @@ func LargestPalindromeProduct(digits int) float64 {
 		}
 	}
 	return largestPalindrome
+}
+
+// LargestPalindromeProduct2 with go routines
+// uses go routines, actually takes longer probably due to switching routines.
+func LargestPalindromeProduct2(digits int) int {
+	runtime.GOMAXPROCS(10)
+	maxStart := 0.0
+	minStart := 0.0
+	ch := make(chan int, 10000)
+	largestPalindrome := 0
+	for i := 0; i < digits; i++ {
+		maxStart += 9.0 * math.Pow(10.0, float64(i))
+		if i == digits-1 {
+			minStart = 1.0 * math.Pow(10.0, float64(i))
+		}
+	}
+	for i := maxStart; i >= minStart; i-- {
+		go determineLargestPalindromeRoutine(int(i), ch)
+	}
+	for j := maxStart; j >= minStart; j-- {
+		number := <-ch
+		if number > largestPalindrome {
+			largestPalindrome = number
+		}
+	}
+	close(ch)
+	return largestPalindrome
+}
+
+func determineLargestPalindromeRoutine(number int, ch chan<- int) {
+	for i := number; i > 0; i-- {
+		currentProduct := number * i
+		isPal := isPalindrome(strconv.Itoa(currentProduct))
+		if isPal {
+			ch <- currentProduct
+			return
+		}
+	}
+	ch <- 0
+	return
 }
 
 func isPalindrome(str string) bool {
--- largestPalindromeProduct_test.go
+++ largestPalindromeProduct_test.go
@@ -16,20 +16,50 @@ func TestLargestPalindromeProduct(t *testing.T) {
 	}
 }
 
-func BenchmarkLargestPalindromeProduct1(b *testing.B) {
+func TestLargestPalindromeProduct2(t *testing.T) {
+	result := LargestPalindromeProduct2(2)
+	if result != 9009 {
+		t.Errorf("%d, does not equal %d", result, 9009)
+	}
+
+	result = LargestPalindromeProduct2(1)
+	if result != 9 {
+		t.Errorf("%d, does not equal %d", result, 9)
+	}
+}
+
+func BenchmarkLargestPalindromeProduct1_1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		LargestPalindromeProduct(1)
 	}
 }
 
-func BenchmarkLargestPalindromeProduct2(b *testing.B) {
+func BenchmarkLargestPalindromeProduct1_2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		LargestPalindromeProduct(2)
 	}
 }
 
-func BenchmarkLargestPalindromeProduct3(b *testing.B) {
+func BenchmarkLargestPalindromeProduct1_3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		LargestPalindromeProduct(3)
+	}
+}
+
+func BenchmarkLargestPalindromeProduct2_1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		LargestPalindromeProduct2(1)
+	}
+}
+
+func BenchmarkLargestPalindromeProduct2_2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		LargestPalindromeProduct2(2)
+	}
+}
+
+func BenchmarkLargestPalindromeProduct2_3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		LargestPalindromeProduct2(3)
 	}
 }
